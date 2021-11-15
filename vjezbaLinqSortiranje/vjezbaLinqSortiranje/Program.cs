2ab65c5e380ec3914d61e5e29c544eb919b5689d
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-11-15 12:26:17 +0100

    Add files via upload

---
+++ vjezbaLinqSortiranje/vjezbaLinqSortiranje/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Student
+{
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+    public int Age { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student[] studentNiz = {
+            new Student() { StudentID = 1, StudentName = "John", Age = 18 },
+            new Student() { StudentID = 2, StudentName = "Steve",  Age = 21 },
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 25 },
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 31 },
+            new Student() { StudentID = 6, StudentName = "Chris",  Age = 17 },
+            new Student() { StudentID = 7, StudentName = "Rob",Age = 19  },
+        };
+
+        var student = from s in studentNiz
+                      where s.Age > 12 && s.Age < 20
+                      orderby s.StudentName descending
+                      select s;
+
+
+    
+
+        foreach (var s in student)
+        {
+            Console.WriteLine(s.StudentName);
+        }
+        
+
+        Console.ReadKey();
+
+    }
+}
